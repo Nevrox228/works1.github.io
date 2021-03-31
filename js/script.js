08e461509552064240fc5ff88d91e230cc708d3f
Nevrox228 <75831200+Nevrox228@users.noreply.github.com>
2021-04-01 01:43:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){   
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    }); 
+
+});  

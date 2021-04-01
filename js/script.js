478faf14d3705f030453dfcb8e79206c2c926695
Nevrox228 <75831200+Nevrox228@users.noreply.github.com>
2021-04-02 00:13:32 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ $(document).ready(function(){
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
-    }); 
+    });    
+
+    $('.hamburger').click(function (event) {
+        $('.menu ').toggleClass('menu_active');
+        $('.hamburger ').toggleClass('hamburger_active');
+    });
 
 });  

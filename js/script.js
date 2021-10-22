4ee0b6856a80927e83e6c82c932b4e64150aae09
Nevrox228 <75831200+Nevrox228@users.noreply.github.com>
2021-10-22 02:20:41 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ closeElem.addEventListener('click', () => {
 });
 
 
-const button__mini = document.querySelector('.function__btn__mini'),
+const button__mini = document.querySelector('.blitz__mini__btn'),
 	  info__mini = document.querySelector('.info__mini'),
 	  closeElem__mini = document.querySelector('.info__mini__close');
 
@@ -23,17 +23,67 @@ closeElem__mini.addEventListener('click', () => {
 	info__mini.classList.remove('info__mini__active');
 });
 
-const anchors = document.querySelectorAll('a[href*="#"]')
+// габургер
+
+const hamburger = document.querySelector('.header__mobi__humburger'),
+      menu = document.querySelector('.header__mobi__left'),
+      closeElem2 = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('header__mobi__left__active');
+});
+
+// closeElem.addEventListener('click', () => {
+//     menu.classList.remove('active');
+// });
+
+// скролл
+
+const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
-    e.preventDefault()
+    e.preventDefault();
     
-    const blockID = anchor.getAttribute('href').substr(1)
+    const blockID = anchor.getAttribute('href').substr(1);
     
     document.getElementById(blockID).scrollIntoView({
       behavior: 'smooth',
       block: 'start'
-    })
-  })
+    });
+  });
 }
+
+
+// $(document).ready(function () {
+//     $('.impossible__wrapper').slick({
+//       infinite: true,
+//       slidesToShow: 1,
+//       slidesToScroll: 1
+//     });
+// });
+
+
+
+
+// слайдер
+
+// let offset = 0;  //смещение от левого края  
+// const sliderWrapper = document.querySelector('.impossible__wrapper');
+
+// document.querySelector('.impossible__next').addEventListener('click', function () {
+//   offset = offset + 170;
+//   if (offset > 340) {
+//     offset = 0;
+//   }
+//   sliderWrapper.style.left = offset + 'px';
+// });
+
+// document.querySelector('.impossible__prev').addEventListener('click', function () {
+//   offset = offset - 170;
+//   if (offset < 0) {
+//     offset = 340;
+//   }
+//   sliderWrapper.style.left = offset + 'px';
+// });
+

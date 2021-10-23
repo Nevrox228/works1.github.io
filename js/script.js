f0c013340205a2705eb982e4d060ac0a649f139b
Nevrox228 <75831200+Nevrox228@users.noreply.github.com>
2021-10-23 19:33:24 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -104,7 +104,9 @@ for (let anchor of anchors) {
 
 $(document).ready(function () {
     $('.impossible__wrapper__2').slick({
-     
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/next.png"></button>',      
+      autoPlaySpedd: 5000
     });
 });
 

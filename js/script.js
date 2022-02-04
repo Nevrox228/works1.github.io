6e01d07aa9a124210cddd2bd3b00c906265374b2
Nevrox228 <75831200+Nevrox228@users.noreply.github.com>
2022-02-04 10:41:29 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,29 +1,52 @@
-// "use strict"
+window.addEventListener('DOMContentLoaded', () => { 
 
-
-
-const hamburger = document.querySelector('.header__top__burger'),
+    const hamburger = document.querySelector('.header__top__burger'),
       menu = document.querySelector('.header__sidepanel'),
+      closeElemSm = document.querySelector('.header__close__sm'),
       closeElem = document.querySelector('.header__close');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('header__sidepanel__active');
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('header__sidepanel__active');
+        document.body.style.overflow = 'hidden';
+    });
+
+    closeElemSm.addEventListener('click', () => {
+        closeElemSm.classList.toggle('header__close__sm__active');
+        menu.classList.toggle('header__sidepanel__active');
+        document.body.classList.toggle('hidden');
+    });
+
+    closeElem.addEventListener('click', () => {
+        menu.classList.remove('header__sidepanel__active');
+
+        document.body.style.overflow = 'auto';
+    });
+
+
+    const searchIcon = document.querySelector('.header__top__search__input__img'),
+          form = document.querySelector('.header__top__search__input'),
+          body = document.querySelector('.body'),
+          headerTopSocial = document.querySelector('.header__top__social'),
+          headerTopLogin = document.querySelector('.header__top__login');
+
+          
+    searchIcon.addEventListener('click', () => {
+        form.classList.toggle('header__top__search__input__active');
+        body.classList.add('body__active');
+        headerTopSocial.style.display = 'none';
+        headerTopLogin.style.display = 'none';
+    });
+
+
+    body.addEventListener('click', (e) => {
+		if (e.target === body) {
+            console.log('nkml');
+        form.classList.remove('header__top__search__input__active');
+        body.classList.remove('body__active');
+        headerTopSocial.style.display = '';
+        headerTopLogin.style.display = '';
+		}
+	});
+
 });
 
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('header__sidepanel__active');
-});
-
-
-
-// const hamburger1 = document.querySelector('.header__top__burger'),
-//       menu1 = document.querySelector('.header__sidepanel'),
-//       closeElem1 = document.querySelector('.menu__close');
-
-// hamburger1.addEventListener('click', () => {
-//     menu.classList.add('active');
-// });
-
-// closeElem1.addEventListener('click', () => {
-//     menu.classList.remove('active');
-// });
